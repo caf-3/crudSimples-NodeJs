2d1d206dcc03448bbb623456cb3ecb1e867840c6
Tomas Caetano <65103987+caf-3@users.noreply.github.com>
2020-09-07 07:52:11 +0200

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV == 'production'){
-    module.exports = {mongoURI : 'YOURATLASURI'}
+    module.exports = {mongoURI : 'yourAtlasURI'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/js-community'}
 }
